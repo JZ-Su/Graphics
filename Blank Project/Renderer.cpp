fda13950e912a4096df9e06a970348a177a99405
Junzhi Su <c3001397@newcastle.ac.uk>
2023-11-10 01:17:05 +0000

    sphere render

--- Blank Project/Renderer.cpp
+++ Blank Project/Renderer.cpp
@@ -1,30 +1,53 @@
 #include "Renderer.h"
+#include "../nclgl/Camera.h"
 
 Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
-	triangle = Mesh::GenerateTriangle();
+	//triangle = Mesh::GenerateTriangle();
+	sphere = Mesh::LoadFromMeshFile("Sphere.msh");  //numVertices = 515
 
-	basicShader = new Shader("basicVertex.glsl","colourFragment.glsl");
+	basicShader = new Shader("SceneVertex.glsl","SceneFragment.glsl");
+	camera = new Camera();
 
 	if(!basicShader->LoadSuccess()) {
 		return;
 	}
 
+	texture = SOIL_load_OGL_texture(TEXTUREDIR"noise.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+	if (!texture) {
+		return;
+	}
+
+	projMatrix = Matrix4::Perspective(1.0f, 10000.0f, (float)width / (float)height, 45.0f);
+	camera->SetPosition(Vector3(0, 0, 10));
+	glEnable(GL_DEPTH_TEST);
 	init = true;
 }
 Renderer::~Renderer(void)	{
-	delete triangle;
+	delete sphere;
 	delete basicShader;
 }
 
 void Renderer::UpdateScene(float dt) {
-
+	camera->UpdateCamera(dt);
+	viewMatrix = camera->BuildViewMatrix();
 }
 
 void Renderer::RenderScene()	{
-	glClearColor(0.2f,0.2f,0.2f,1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 
 	BindShader(basicShader);
-	triangle->Draw();
+	UpdateShaderMatrices();
+	modelMatrix = Matrix4::Translation(Vector3(0, 0, 0)) * Matrix4::Scale(Vector3(10, 10, 10)) * Matrix4::Rotation(0, Vector3(1, 1, 1));
+	Vector4 nodeColour = Vector4(1.0f, 0.1f, 0.0f, 1.0f);
+
+	//triangle->Draw();
+	glUniform1i(glGetUniformLocation(basicShader->GetProgram(), "diffuseTex"), 0);
+	glUniform1i(glGetUniformLocation(basicShader->GetProgram(), "useTexture"), 1);
+	glUniform4fv(glGetUniformLocation(basicShader->GetProgram(), "nodeColour"), 1, (float*)&nodeColour);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	sphere->Draw();
 }
 
--- Blank Project/Renderer.h
+++ Blank Project/Renderer.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "../NCLGL/OGLRenderer.h"
+class Camera;
 
 class Renderer : public OGLRenderer	{
 public:
 	Renderer(Window &parent);
 	 ~Renderer(void);
 	 void RenderScene()				override;
-	 void UpdateScene(float msec)	override;
+	 void UpdateScene(float dt)		override;
 protected:
 	Mesh*	triangle;
+	Mesh*	sphere;
 	Shader* basicShader;
+	Camera* camera;
+	GLuint  texture;
 };
